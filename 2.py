59be299cae6b13eb759dbdd98a40cec1967cc3e0
vvlera <114632272+vvlera@users.noreply.github.com>
2022-09-29 09:21:36 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,2 @@
+for x in range(p-1)
+for y in range(p-1)
---
+++ Уйманова.py
@@ -0,0 +1,12 @@
+p = int(input('p='))
+Valid = p
+Np = int(input(f'N{p}='))
+k = int(1)
+N10= int(0)
+while not Np ==0:
+    N10= N10 + (Np%10) * k
+    k= k*p
+    Np = Np//10
+print(f'N10 = {N10}=')
+
+
