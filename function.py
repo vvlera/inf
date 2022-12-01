07fa7f5e85f238e86857f0c753b08eae4ac48de0
Уйманова Валерия <114632272+vvlera@users.noreply.github.com>
2022-12-01 09:14:52 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,7 @@
+def f(a):
+    b=0
+    b= b+1
+    print(b)
+a='2'
+f(a)
+print(b)
---
+++ list.py
@@ -0,0 +1,11 @@
+a=['2','3']
+if
+input()
+print()
+a=b=0
+a,b=b,a
+for i in range(len(a)): #easier
+    a=[i]
+for i in a:
+    
+    
