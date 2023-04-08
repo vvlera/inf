f746f58fa4c5ad8d822373ad887cd5a6ea50681d
Уйманова Валерия <114632272+vvlera@users.noreply.github.com>
2023-04-08 14:37:05 +0700

    Update mi.py

--- 2полуг/mi.py
+++ 2полуг/mi.py
@@ -6,6 +6,10 @@ for i in range(len(matr[0])): #перебор первого элемента с
     for g in range(len(ch)):
         print(matr[ch[g]][i],end =' ' ) # печать буквы
     print() # переход на следующую букву
+    
+    
+for g in ch:
+    print(matr[g][i],end =' ')
 '''a1 = matr[0]
 a2 = matr[1]
 a3 = matr[2]
